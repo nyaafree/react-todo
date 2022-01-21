95ce457e777569277100b715b9fd59d7582c4ba7
nyaafree <45682975+nyaafree@users.noreply.github.com>
2022-01-22 03:28:09 +0900

    feat: 未完了リストと完了リストの内容をstateで管理するように変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompletetodos, setincompletetodos] = useState(["aaa", "bbb"]);
+  const [completetodos, setcompletetodos] = useState(["ccc"]);
   return (
     <>
       <div className="input-area">
@@ -11,31 +13,37 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p>aaa</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
-          <li>
-            <div className="list-row">
-              <p>bbb</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {/*  
+            Reactではloop処理をする場合は先頭の要素にキーを設定する必要がある
+            理由：　仮想DOMでは変更前と変更後の差分だけを抽出しているため、差分のみを再レンダリングするため
+            再レンダリングされた場合にLoopの何番目の要素なのか判断するための目印としてつける 
+          */}
+          {incompletetodos.map((todo) => {
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p>ccc</p>
-              <button>戻す</button>
-            </div>
-          </li>
+          {completetodos.map((todo) => {
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button>戻す</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
